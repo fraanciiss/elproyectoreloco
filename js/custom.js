f0f227aded5b65d79b2438550105053e7f74df42
LGiardino <ltgiardino@gmail.com>
2016-08-15 12:55:30 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -105,3 +105,18 @@ $(function(){
 
 });
 
+function initialize() {
+	var mapProp = {
+		center:new google.maps.LatLng(-34.591604,-58.394264),
+		zoom:16,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI:true
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var marker=new google.maps.Marker({
+	  position:map.center,
+	});
+
+	marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
